2a13a95dac5fe063e7eb9fd25eb3cb70d31626eb
Jiang Haoyu <144139731+12ded21@users.noreply.github.com>
2024-12-30 13:04:41 +0800

    Over!

--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -1,26 +1,22 @@
 #pragma once
-#include"PoseHandler.hpp"
-namespace adas{
-    class ExecutorImpl final : public Executor{
-    private:
-        PoseHandler poseHandler;
-        std::string kind;
-    //接口抽象，虚基类
-    public:
-        /*void Move(void) noexcept;
-        void TurnLeft(void) noexcept;
-        void TurnRight(void) noexcept;
-        void Fast(void) noexcept;
-        bool IsFast(void) noexcept;*/
+#include "PoseHandler.hpp"
+namespace adas
+{
+class ExecutorImpl final : public Executor
+{
+private:
+    PoseHandler poseHandler;
+    std::string kind;
+    // 接口抽象，虚基类
 
-    public:
-        explicit ExecutorImpl(const Pose& p = {0,0,'N'},const std::string& kind = "car") noexcept;
-        ~ExecutorImpl(void) noexcept = default;
+public:
+    explicit ExecutorImpl(const Pose& p = {0, 0, 'N'}, const std::string& kind = "car") noexcept;
+    ~ExecutorImpl(void) noexcept = default;
 
-        ExecutorImpl(const Executor&) = delete;
-        ExecutorImpl& operator=(const Executor&) = delete;
+    ExecutorImpl(const Executor&) = delete;
+    ExecutorImpl& operator=(const Executor&) = delete;
 
-        void Execute(const std::string& command) noexcept override;
-        Pose Query(void)const noexcept override;
-    };
-}
+    void Execute(const std::string& command) noexcept override;
+    Pose Query(void) const noexcept override;
+};
+}  // namespace adas
